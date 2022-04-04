4d4c536324b2c4e98a611677073ab740058d89cc
Lydia <Utilisateur@LAPTOP-L4HQ889S>
2022-04-04 21:40:49 +0200

    création des entities savons et huiles

---
+++ src/Entity/Huile.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HuileRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=HuileRepository::class)
+ */
+class Huile
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="decimal", precision=4, scale=3)
+     */
+    private $indiceSaponification;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $INS;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $indiceIode;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Savon::class, mappedBy="huiles")
+     */
+    private $savons;
+
+    public function __construct()
+    {
+        $this->savons = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIndiceSaponification(): ?string
+    {
+        return $this->indiceSaponification;
+    }
+
+    public function setIndiceSaponification(string $indiceSaponification): self
+    {
+        $this->indiceSaponification = $indiceSaponification;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getINS(): ?int
+    {
+        return $this->INS;
+    }
+
+    public function setINS(int $INS): self
+    {
+        $this->INS = $INS;
+
+        return $this;
+    }
+
+    public function getIndiceIode(): ?int
+    {
+        return $this->indiceIode;
+    }
+
+    public function setIndiceIode(int $indiceIode): self
+    {
+        $this->indiceIode = $indiceIode;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Savon>
+     */
+    public function getSavons(): Collection
+    {
+        return $this->savons;
+    }
+
+    public function addSavon(Savon $savon): self
+    {
+        if (!$this->savons->contains($savon)) {
+            $this->savons[] = $savon;
+            $savon->addHuile($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSavon(Savon $savon): self
+    {
+        if ($this->savons->removeElement($savon)) {
+            $savon->removeHuile($this);
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Savon.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SavonRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SavonRepository::class)
+ */
+class Savon
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $surgraissage;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $concentrationSoude;
+
+    /**
+     * @ORM\Column(type="decimal", precision=7, scale=1, nullable=true)
+     */
+    private $masseSoude;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $dateCreation;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Huile::class, inversedBy="savons")
+     */
+    private $huiles;
+
+    public function __construct()
+    {
+        $this->huiles = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getSurgraissage(): ?int
+    {
+        return $this->surgraissage;
+    }
+
+    public function setSurgraissage(int $surgraissage): self
+    {
+        $this->surgraissage = $surgraissage;
+
+        return $this;
+    }
+
+    public function getConcentrationSoude(): ?int
+    {
+        return $this->concentrationSoude;
+    }
+
+    public function setConcentrationSoude(int $concentrationSoude): self
+    {
+        $this->concentrationSoude = $concentrationSoude;
+
+        return $this;
+    }
+
+    public function getMasseSoude(): ?string
+    {
+        return $this->masseSoude;
+    }
+
+    public function setMasseSoude(?string $masseSoude): self
+    {
+        $this->masseSoude = $masseSoude;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->dateCreation;
+    }
+
+    public function setDateCreation(\DateTimeInterface $dateCreation): self
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Huile>
+     */
+    public function getHuiles(): Collection
+    {
+        return $this->huiles;
+    }
+
+    public function addHuile(Huile $huile): self
+    {
+        if (!$this->huiles->contains($huile)) {
+            $this->huiles[] = $huile;
+        }
+
+        return $this;
+    }
+
+    public function removeHuile(Huile $huile): self
+    {
+        $this->huiles->removeElement($huile);
+
+        return $this;
+    }
+}
